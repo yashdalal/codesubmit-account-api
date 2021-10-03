cecf7f1a05b9e4c2b1902f7369d51f592cc6c5b2
Yash Dalal <ydalal@amazon.com>
2021-10-03 13:06:49 -0400

    Add no args constructor to enable simpler unit tests

--- account-manager/src/main/java/com/ydalal/accounts/exceptions/AccountNotFoundException.java
+++ account-manager/src/main/java/com/ydalal/accounts/exceptions/AccountNotFoundException.java
@@ -1,6 +1,10 @@
 package com.ydalal.accounts.exceptions;
 
 public class AccountNotFoundException extends Exception {
+    public AccountNotFoundException() {
+        super();
+    }
+
     public AccountNotFoundException(final String errorMessage) {
         super(errorMessage);
     }
--- account-manager/src/main/java/com/ydalal/accounts/exceptions/NotEnoughBalanceException.java
+++ account-manager/src/main/java/com/ydalal/accounts/exceptions/NotEnoughBalanceException.java
@@ -1,6 +1,10 @@
 package com.ydalal.accounts.exceptions;
 
 public class NotEnoughBalanceException extends Exception{
+    public NotEnoughBalanceException() {
+        super();
+    }
+
     public NotEnoughBalanceException(final String errorMessage) {
         super(errorMessage);
     }
