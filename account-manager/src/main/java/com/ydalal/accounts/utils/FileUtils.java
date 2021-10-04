04f402014957f6361986ca5477f8369c074fb9dd
Yash Dalal <ydalal@amazon.com>
2021-10-03 20:43:40 -0400

    Adding a file util class

--- account-manager/src/main/java/com/ydalal/accounts/utils/FileUtils.java
+++ account-manager/src/main/java/com/ydalal/accounts/utils/FileUtils.java
@@ -1,2 +1,9 @@
-package com.ydalal.accounts.utils;public class FileUtils {
+package com.ydalal.accounts.utils;
+
+import java.io.File;
+
+public class FileUtils {
+    public static boolean fileIsEmpty(final File file) {
+        return file.length() == 0;
+    }
 }
